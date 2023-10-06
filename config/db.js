891b62cf615034f3ba36d513ceaabeb885f92932
valtiep1 <valtiep1@gmatc.matc.edu>
2023-10-06 13:47:34 -0500

    deprecation warning fix

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDatabase = async () => {
     
     try{
         await mongoose.connect(db, {
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
         console.log('Connected to MongoDB');
     }catch (error){
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import express, { Router } from 'express';
 import connectDatabase from './config/db';
-import {check, validationResult} from 'express-validator';
+import { check, validationResult } from 'express-validator';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
+import User from './models/User';
 
 const app = express();
 
 connectDatabase();
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 /** 
 * @route GET /
@@ -15,7 +17,7 @@ app.use(express.json({extended: false}));
 */
 
 app.get('/', (req, res) =>
-res.send('http get request sent to root api endpoint'));
+    res.send('http get request sent to root api endpoint'));
 
 /** 
 * @route POST api/users /
@@ -23,26 +25,54 @@ res.send('http get request sent to root api endpoint'));
 */
 
 app.post('/api/users',
-[
-check('name', 'Please enter your name').not().isEmpty(),
-check('email','Please enter a valid email').isEmail(),
-check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-],
-(req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({errors: errors.array()}); 
-    }else{
-        return res.send(req.body);
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { name, email, password } = req.body;
+            try {
+                let user = await User.findOne({ email: email });
+                if (user) {
+                    return res.status(400)
+                        .json({ errors: [{ msg: 'User already exists!' }] });
+                }
+                //Create New User
+                user = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+
+                //Encrypt the password
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                //Save to the db and return
+                await user.save();
+                res.send('User successfully registered!');
+
+            }
+            catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
     }
-});
+);
 
 const port = 5000;
 app.listen(port, () => console.log(`Express server running on port ${port}`));
 
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 app.use(
     cors({
-        origin: 'http//localhost3000'})
+        origin: 'http//localhost3000'
+    })
 );
- 
+
